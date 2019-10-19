89f08038cb6feec72c7a91e9b8f130cf0555dd98
Чекушин Андрей <bro.chek@mail.ru>
2019-10-19 19:15:52 +0300

    Добавил wpf окно в папку Lab_1

---
+++ Lab_1/Lab_1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp_Labs.Lab_1
+{
+    /// <summary>
+    /// Логика взаимодействия для Lab_1.xaml
+    /// </summary>
+    public partial class Lab_1 : Window
+    {
+        public Lab_1()
+        {
+            InitializeComponent();
+        }
+    }
+}
